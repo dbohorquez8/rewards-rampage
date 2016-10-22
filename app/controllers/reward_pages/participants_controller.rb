f0de62b80eb7ca92201afcb37842749fa68139b5
Miguel Perez <miguel.perez@koombea.com>
2016-10-22 16:10:17 -0500

    adding participants

--- app/controllers/reward_pages/participants_controller.rb
+++ app/controllers/reward_pages/participants_controller.rb
@@ -6,16 +6,20 @@ class RewardPages::ParticipantsController < ApplicationController
   def create
     @participant = current_reward_page.participants.build(participant_params)
 
-    if @participant.save
-      redirect_to reward_page_path(id: current_reward_page.identifier)
-    else
-      render "new"
+    respond_to do |format|
+      if @participant.save
+        format.html { redirect_to reward_page_path(current_reward_page.identifier) }
+        format.js { }
+      else
+        format.html { render action: "new" }
+        format.js { }
+      end
     end
   end
 
   private
 
   def participant_params
-    params.require(:participant).permit(:name)
+    params.require(:participant).permit(:name, :email)
   end
 end
