5ef61d1e7a864fd4e48a74cde326d52440275954
German Garcia <german.garcia.d@gmail>
2016-10-22 18:36:19 -0500

    Adds photo uploads

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,10 +3,8 @@ class Reward < ApplicationRecord
 
   belongs_to :reward_page
 
-  # mount_uploader :photo, PhotoUploader
-
   validates :name, presence: true
-  # validates :photo, presence: true
+  validates :photo, presence: true
   validates :points, presence: true, numericality: true
   validates :reward_page_id, presence: true
 
