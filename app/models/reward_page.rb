248129ceb22ef8e927500fcc139120b119182fe7
Miguel Perez <miguel.perez@koombea.com>
2016-10-22 10:34:17 -0500

    add reward-page model.

---
+++ app/models/reward_page.rb
@@ -0,0 +1,13 @@
+class RewardPage < ApplicationRecord
+  validates :name, presence: true
+  validates :identifier, presence: true
+  validates :identifier, uniqueness: true
+
+  before_validation :generate_identifier, on: :create
+
+  private
+
+  def generate_identifier
+    self.identifier = SecureRandom.hex(16) unless self.identifier
+  end
+end
