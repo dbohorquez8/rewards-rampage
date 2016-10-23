cc6fade4a833689e4a32d3e106bdaf8434c48cee
Miguel Perez <miguel.perez@koombea.com>
2016-10-22 22:25:07 -0500

    add owner email to rewards pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022224106) do
+ActiveRecord::Schema.define(version: 20161023032427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20161022224106) do
   create_table "reward_pages", force: :cascade do |t|
     t.string   "name"
     t.string   "identifier"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "owner_email"
     t.index ["identifier"], name: "index_reward_pages_on_identifier", unique: true, using: :btree
   end
 
