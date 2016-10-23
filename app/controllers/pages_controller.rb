049d0cd3aa0d7cc1e05fa78bc7291eb50d58f955
German Garcia <german.garcia.d@gmail>
2016-10-22 23:12:58 -0500

    Fixes web address upload

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,18 @@ class PagesController < ApplicationController
   end
 
   def signature
-  	render :nothing and return unless params[:data].present?
+  	render :nothing and return unless data.present?
   	render text: Digest::SHA1.hexdigest(string_to_sign)
   end
 
   private
 
   def string_to_sign
-	return "" unless params[:data].present?
-	"#{params[:data].sort.map {|key, val| "#{key}=#{val}"}.join('&')}#{ENV['cloudinary_api_secret']}"
+	return "" unless data.present?
+	"#{data.except('file').sort.map {|key, val| "#{key}=#{val}"}.join('&')}#{ENV['cloudinary_api_secret']}"
+  end
+
+  def data
+  	@data ||= params[:data]
   end
 end
