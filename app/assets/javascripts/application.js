1099695ad07d8aa193c04949c0916018011e3fb3
Miguel Perez <miguel.perez@koombea.com>
2016-10-23 13:05:51 -0500

    adding helper cues to show page of users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,38 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require chardinjs.min
+//= require js.storage.min
 //= require bootstrap
 //= require theme/custom
+
+// This object will help us calling a callback depending on wether or not
+// it has been called before. For this it uses a key, if the key is set
+// then the callback will not be called.
+NotificationsHelper = (function(){
+  var $storage = null;
+
+  function never_called(key){
+    return $storage.get(key) === false ||
+      $storage.get(key) === undefined ||
+      $storage.get(key) == null;
+  }
+
+  function init(key, callback){
+    $storage = Storages.localStorage;
+
+    // checks if the key is set
+    if(never_called(key)){
+      callback.call();
+      $storage.set(key, true);
+    }
+  }
+
+  function mami(){
+    console.log("que quieres mijo?");
+  }
+
+  return {
+    init: init,
+    mami: mami
+  }
+})();
